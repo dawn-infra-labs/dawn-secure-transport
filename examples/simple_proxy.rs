cbe769657f8216f574f577e4e8b09af05617e1a2
folk307 <folkdivine@outlook.com>
2026-02-05 04:30:24 +0800

    Delete examples/simple_proxy.rs

--- examples/simple_proxy.rs
+++
@@ -1,6 +0,0 @@
-//! A minimal proxy example demonstrating transports + routing
-
-fn main() {
-    println!("Starting proxy example...");
-    // TODO: Initialize transports, load configuration, start listener
-}
